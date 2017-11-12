1ced4195eaec8654a7a4d5e244a19bc7e9302626
Eugeny Gostkin <gostkin.em@phystech.edu>
2017-11-12 07:39:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,4 +9,5 @@ $(function(){
     $('#root').append('<ul></ul>');
     $('#root').append('<input id="add_task_input"> <button id="add_task">Добавить</button>');
     $('#add_task').click(function(){addElement($('#add_task_input').val())});
+    addElement("Сделать задание #3 по web-программированию");
 })
