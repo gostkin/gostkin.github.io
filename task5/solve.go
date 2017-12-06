3bdf53c97d72f38b05575f91c13902ad9a1e4bb4
Eugeny Gostkin <gostkin.em@phystech.edu>
2017-12-06 14:11:56 +0300

    Create solve.go

---
+++ task5/solve.go
@@ -0,0 +1,59 @@
+package main
+
+import "encoding/json"
+import "io/ioutil"
+import "log"
+import "net/http"
+import "strconv"
+
+import "github.com/gorilla/mux"
+
+var urls map[string]string
+
+func nextKey() string {
+	return strconv.Itoa(len(urls))
+}
+
+func getRedirect(writer http.ResponseWriter, request *http.Request) {
+	key := mux.Vars(request)["key"]
+	url, res := urls[key]
+	
+	if !res {
+		http.NotFound(writer, request)
+		return
+	}
+	
+	http.Redirect(writer, request, url, 301)
+}
+
+func addLink(writer http.ResponseWriter, request *http.Request) {
+	defer request.Body.Close()
+	
+	var body []byte
+	body, _ = ioutil.ReadAll(request.Body)
+	
+	var rjson map[string]string
+	if error := json.Unmarshal(body, &rjson); error != nil {
+		panic(error)
+	}
+	
+	url := rjson["url"]
+	
+	key := nextKey()
+	urls[key] = url
+
+	rjson := map[string]string {"key": key}
+	result, _ := json.Marshal(rjson)
+	writer.Write(result)
+}
+
+func main() {
+	urls = make(map[string]string)
+
+	r := mux.NewRouter()
+  
+	r.HandleFunc("/{key}", getRedirect).Methods("GET")
+	r.HandleFunc("/", addLink).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8082", r))
+}
